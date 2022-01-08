8888cedbbb3533ed325fc24aa4ab911ec175fd53
LudovicAL <ludovicaubut@hotmail.com>
2022-01-08 01:12:14 -0500

    Second attempt at toutchscreen controls

--- Assets/Scripts/CanvasBehavior.cs
+++ Assets/Scripts/CanvasBehavior.cs
@@ -22,7 +22,7 @@ public class CanvasBehavior : MonoBehaviour {
 		aboutPanel = transform.Find("Panel About").gameObject;
 		musicAudioSource = transform.Find("Toggle Music").GetComponent<AudioSource>();
 		ShowAboutPanel(false);
-		if (!Input.touchSupported) {
+		if (Application.platform != RuntimePlatform.Android && Application.platform != RuntimePlatform.IPhonePlayer) {
 			transform.Find("Panel Game").gameObject.SetActive(false);
 		}
 	}
